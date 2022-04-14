5eb16417cb7178ac480ca1de86b504a9d33f6890
JHopeCollins <jhc.jss@gmail.com>
2022-04-14 18:02:08 +0100

    swe:williamson: optional arguments for parameters of case2 profiles

--- firedrake_utils/shallow_water/williamson1992/case2.py
+++ firedrake_utils/shallow_water/williamson1992/case2.py
@@ -29,16 +29,16 @@ def coriolis_function( x,y,z, Vf, name="coriolis" ):
     return fd.Function( Vf, name=name ).interpolate(coriolis_expression(x,y,z))
 
 # velocity field u
-def velocity_expression( x,y,z ):
-    return fd.as_vector([ -u0*y/earth.radius, u0*x/earth.radius, 0.0 ])
+def velocity_expression( x,y,z, uref=u0 ):
+    return fd.as_vector([ -uref*y/earth.radius, uref*x/earth.radius, 0.0 ])
 
-def velocity_function( x,y,z, V1, name="velocity" ):
-    return fd.Function( V1, name=name ).project(velocity_expression(x,y,z))
+def velocity_function( x,y,z, V1, uref=u0, name="velocity" ):
+    return fd.Function( V1, name=name ).project(velocity_expression(x,y,z,uref=uref))
 
 # depth field h
-def depth_expression( x,y,z ):
-    return h0 - ( earth.radius*earth.omega*u0 + 0.5*u0*u0 )*(z*z/(earth.radius*earth.radius))/earth.gravity
+def depth_expression( x,y,z, href=h0, uref=u0 ):
+    return href - ( earth.radius*earth.omega*uref + 0.5*uref*uref )*(z*z/(earth.radius*earth.radius))/earth.gravity
 
-def depth_function( x,y,z, V2, name="depth" ):
-    return fd.Function( V2, name=name ).interpolate(depth_expression(x,y,z))
+def depth_function( x,y,z, V2, href=h0, uref=u0, name="depth" ):
+    return fd.Function( V2, name=name ).interpolate(depth_expression(x,y,z,href=href,uref=uref))
 
