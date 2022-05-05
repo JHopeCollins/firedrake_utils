7d99c6c1d862058fa685d26ec65ec067d0688ad5
JHopeCollins <jhc.jss@gmail.com>
2022-05-05 13:57:52 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='firedrake_utils',
+      packages=['firedrake_utils'],
+      author='Josh Hope-Collins',
+      author_email='jth113@ic.ac.uk',
+      description='Some small utilities to be used with Firedrake')
